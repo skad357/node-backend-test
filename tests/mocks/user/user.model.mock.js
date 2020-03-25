24ad6bb13ffddcab61529a0ce39e7526363f7cb4
Jorge Jaulis <jjaulisr@gmail.com>
2020-03-25 14:35:33 -0500

    Add unit tests

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,40 @@
+module.exports = {
+    user: {
+        "_id": "5e7ab1d62ce6f03e266f69bd",
+        "name": "skad",
+        "username": "jjaulis",
+        "__v": 0
+    },
+    users: [
+        {
+            "_id": "5e7ab1d62ce6f03e266f69bd",
+            "name": "skad",
+            "username": "jjaulis",
+            "__v": 0
+        },
+        {
+            "_id": "5e7b824540cbe5599fc36545",
+            "name": "JOSUE",
+            "username": "markap",
+            "__v": 0
+        },
+        {
+            "_id": "5e7b825a40cbe5599fc36546",
+            "name": "laiko",
+            "username": "lc300",
+            "__v": 0
+        },
+        {
+            "_id": "5e7b826d40cbe5599fc36547",
+            "name": "percusion",
+            "username": "perk666",
+            "__v": 0
+        },
+        {
+            "_id": "5e7b828740cbe5599fc36548",
+            "name": "Juan Villa",
+            "username": "lastTest",
+            "__v": 0
+        }
+    ]
+}
